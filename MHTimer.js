ea0d5dddfb204fb3ad2bad2f259bf30b5d81fe49
Rob Wilson <aardwolf98@gmail.com>
2017-07-20 17:21:23 -0400

    Change ping to -gate, timers smarter

--- MHTimer.js
+++ MHTimer.js
@@ -4,6 +4,36 @@
 
 // Import the discord.js module
 const Discord = require('discord.js');
+var Timers = require('./timers.js');
+var fs = require ('fs');
+
+
+//A test to add a timer
+var timers = [];
+// var timers = [new Timers('gate', 1500573088000, 86400000, "Test timer is happening")];
+//Read it from a file
+var obj;
+fs.readFile('timers.json', 'utf8', function readFileCallback(err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		obj = JSON.parse(data);
+		for (var i = 0; i < obj.length; i++ ) {
+//		console.log('obj length' + obj.length);
+			timers.push(new Timers(obj[i].area, obj[i].seed_time, obj[i].repeat_time, obj[i].announce_string));
+		}
+
+//		var str = JSON.stringify(timers, ['area', 'seed_time', 'repeat_time', 'announce_string'], 1);
+//		fs.writeFile('timers.json', str, 'utf8', function writeCallback(err, data){
+//			if (err) {
+//				console.log(err);
+//			}
+//		});
+	}
+});
+
+
+
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
@@ -34,9 +64,13 @@ client.on('ready', () => {
 // Create an event listener for messages
 client.on('message', message => {
   // If the message is "ping"
-  if (message.content === 'ping') {
+  if (message.content === '-gate') {
     // Send "pong" to the same channel
-    message.channel.send('pong');
+//    message.channel.send('pong');
+    message.channel.send('next ' + timers[0].area + ' time is ' + timers[0].getNext().toUTCString());
+    message.channel.send('That would be ' + timers[0].getNext() );
+//    message.channel.send('I know about ' + timers.length + ' timers');
+//    message.channel.send('The first one is ' + timers[0].area);
   }
 });
 
--- timers.js
+++ timers.js
@@ -1,25 +1,23 @@
 // timers.js
 //   Define what a mousehunt timer is.
 
-var Timer = function(area, next_time, repeat_time, announce_string) {
+var Timer = function(area, seed_time, repeat_time, announce_string) {
 	//Simple constructor. Takes input arguments and gives them names, sets a few next times
 	this.area = area;
-	this.next_time = new Date(next_time); //milliseconds since epoch, UTC
+	this.seed_time = new Date(seed_time); //milliseconds since epoch, UTC
 	this.repeat_time = repeat_time; // milliseconds between repeats
 	this.announce_string = announce_string;
 	
-//	this.repeat_time = this.repeat_days * 86400000;
-	
-	//Calculate a few in advance
-	this.futures = [this.next_time, new Date (this.next_time.valueOf() + this.repeat_time) ];
-	this.futures.push(new Date(this.next_time.valueOf() + this.repeat_time * 2));
-	this.futures.push(new Date(this.next_time.valueOf() + this.repeat_time * 3));
-	this.futures.push(new Date(this.next_time.valueOf() + this.repeat_time * 4));
 }
 
-Timer.prototype.advance = function() {
-	//Returns the time that should be old but it doesn't check to make sure
-	this.futures.push(new Date(this.next_time.valueOf() + this.repeat_time * 4));
-	return this.futures.shift();
+Timer.prototype.getNext = function() {
+	var cur_time = Date.now();
+	return new Date(cur_time + ((this.seed_time.valueOf() - cur_time) % this.repeat_time));  //division is numer of whole elapsed iterations, modulus is ms until next
+//	return new Date(cur_time);
 }
-module.export Timer;
+
+Timer.prototype.getAnnounce = function() {
+	return this.announce_string;
+}
+
+module.exports = Timer;
