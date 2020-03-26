3ffa98a42779b12a6de7fbd3df6e3b661d07869a
tehhowch <tehhowch@users.noreply.github.com>
2020-03-25 19:34:56 -0500

    refactor(test): demo nested suite
     - TAP format is not prettied, by design, so it won't appear indented

--- tests/modules/timerClass.js
+++ tests/modules/timerClass.js
@@ -4,16 +4,26 @@ const test = require('tape');
 // Functionality to be tested.
 const Timer = require('../../src/modules/timerClass');
 
-test('Timer ctor - given no seed - throws', t => {
-    t.plan(1);
-    t.throws(() => new Timer());
+test('Timer ctor', function (suite) {
+    suite.test('given no seed - throws', t => {
+        t.plan(1);
+        t.throws(() => new Timer(), TypeError, 'requires input');
+    });
+    suite.test('given improper seed - throws', t => {
+        const plan = [
+            { obj: { key: 'some value' }, error: TypeError, msg: 'not given required keys' },
+            { obj: { area: 'mars', seed_time: 0, repeat_time: 60001 }, error: TypeError, msg: 'given invalid seed_time format' },
+            { obj: { area: 'mars', seed_time: '1970', repeat_time: 1 }, error: RangeError, msg: 'given short repeat' },
+            { obj: { area: 'mars', seed_time: '1970', repeat_time: { seconds: 1 } }, error: RangeError, msg: 'given short luxon repeat' },
+        ];
+        t.plan(plan.length);
+        plan.forEach(({ obj, error, msg }) => t.throws(() => new Timer(obj), error, msg));
+    });
+    suite.test('given valid area, seed_time, repeat_time - does not throw', t => {
+        t.plan(1);
+        t.doesNotThrow(() => new Timer({ area: 'mars', seed_time: '1970-01-01', repeat_time: 60000 }));
+    });
 });
-test('Timer ctor - given improper seed - throws', t => {
-    t.plan(1); // update number
-    t.throws(() => new Timer({ key: 'some value' }));
-    // add other invalid area, seed_time, repeat_time combos that will fail
-});
-test('Timer ctor - given area, seed_time, repeat_time - does not throw', t => {
-    t.plan(1);
-    t.doesNotThrow(() => new Timer({ area: 'mars', seed_time: '1970-01-01', repeat_time: 60001 }));
+test.skip('Other suite', function (suite) {
+    // TODO
 });
