979a896a7397755b3e87e13146ba5bb6d0b31f1c
tehhowch <tehhowch@users.noreply.github.com>
2021-10-14 19:14:14 -0500

    fix(config): "timers add <channel>" works again
    v13 method update, plus simplify logic.

--- src/commands/config.js
+++ src/commands/config.js
@@ -104,23 +104,18 @@ async function doSet(message, tokens) {
             subAction = tokens.shift().toLowerCase();
         if (subAction === 'add') {
             // Next argument should be a channel reference, add it to the array of timer channels.
-            const channels = message.mentions.channels.values();
-            if (channels && channels.length > 0) {
-                const channel = channels.shift();
-                if (!channel)
-                    reply = 'I don\'t think you gave me a channel to add';
-                else if (channel.type !== 'text')
-                    reply = `Didn't add ${channel.toString()} because it's not a text channel`;
-                else if (!channel.name)
-                    reply = `Didn't add ${channel.toString()} because I couldn't figure out its name`;
-                else if (guildSettings.timedAnnouncementChannels.has(channel.name))
-                    reply = `Didn't add ${channel.name} because it's already in the list`;
-                else {
-                    guildSettings.timedAnnouncementChannels.add(channel.name);
-                    reply = `I added ${channel.name} but because Aard is lazy it won't be used until next restart`;
-                }
-            } else {
-                reply = 'I only work with mentions of channels and none were mentioned';
+            const [channel] = Array.from(message.mentions.channels.values());
+            if (!channel)
+                reply = 'I don\'t think you gave me a channel to add';
+            else if (!channel.isText())
+                reply = `Didn't add ${channel.toString()} because "${channel.type}" is not a text channel`;
+            else if (!('name' in channel))
+                reply = `Didn't add ${channel.toString()} because it has no name`;
+            else if (guildSettings.timedAnnouncementChannels.has(channel.name))
+                reply = `Didn't add ${channel.name} because it's already in the list`;
+            else {
+                guildSettings.timedAnnouncementChannels.add(channel.name);
+                reply = `I added ${channel.name} but because Aard is lazy it won't be used until next restart`;
             }
         }
         else if (subAction === 'remove') {
@@ -165,7 +160,7 @@ async function doSet(message, tokens) {
                 await message.channel.send(msg);
             }
             theResult.replied = true;
-            if (message.channel.type === 'dm') theResult.sentDm = true;
+            if (message.channel.type === 'DM') theResult.sentDm = true;
             theResult.success = true;
         } catch (err) {
             Logger.error('IAM: failed to send reply', err);
